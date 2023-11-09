08e8a9be8daab685e7727f223fda877aca627bcc
CodeScott00 <dougal_00@hotmail.com>
2023-11-09 12:07:24 +1100

    git check

--- src/Components/ListGroup.tsx
+++ src/Components/ListGroup.tsx
@@ -7,7 +7,6 @@ function ListGroup() {
         <li className="list-group-item">A second item</li>
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
       </ul>
     </>
   );
